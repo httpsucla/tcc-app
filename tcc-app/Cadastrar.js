549f44343e79944107b98f59c1fcdea23c3f0032
httpsucla <paulasucla@hotmail.com>
2022-07-27 19:38:18 -0300

    adição de mascara nos campos

--- tcc-app/Cadastrar.js
+++ tcc-app/Cadastrar.js
@@ -1,9 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { TextInputMask } from 'react-native-masked-text'
 
 export default function Cadastrar( {navigation} ) {
      
+    const [hour, setHour] = useState('');
+    const [qtde, setQtde] = useState('');
+    const [date, setDate] = useState('');
+    const [days, setDays] = useState('');
+
     async function handleButtonPress(){ 
         navigation.navigate("Gaveta");
     };
@@ -18,29 +24,47 @@ export default function Cadastrar( {navigation} ) {
                         placeholder="Nome"
                         clearButtonMode="always" 
                     /> 
-                    <TextInput 
+                    <TextInputMask 
                         style={styles.input} 
                         placeholder="Quantidade" 
                         keyboardType={'numeric'}
-                        clearButtonMode="always" 
+                        clearButtonMode="always"
+                        type={'only-numbers'}
+                        value={qtde}
+                        onChangeText={ value => setQtde(value) }
                     /> 
-                    <TextInput
+                    <TextInputMask
                         style={styles.input}  
                         placeholder="Horário" 
                         keyboardType={'numbers-and-punctuation'}
                         clearButtonMode="always"
+                        type={'datetime'}
+                        options={{
+                            format: 'HH:mm'
+                        }}
+                        value={hour}
+                        onChangeText={ value => setHour(value) }
                     />
-                    <TextInput
+                    <TextInputMask
                         style={styles.input}  
-                        placeholder="Data inicial" 
+                        placeholder="Data de início" 
                         keyboardType={'numbers-and-punctuation'}
                         clearButtonMode="always"
-                    />
-                    <TextInput
+                        type={'datetime'}
+                        options={{
+                            format: 'dd/MM/YYYY'
+                        }}
+                        value={date}
+                        onChangeText={ value => setDate(value) }
+                    />           
+                    <TextInputMask
                         style={styles.input}  
                         placeholder="Quantidade de dias" 
                         keyboardType={'numeric'}
                         clearButtonMode="always"
+                        type={'only-numbers'}
+                        value={days}
+                        onChangeText={ value => setDays(value) }
                     />
                     <TouchableOpacity style={styles.button} onPress={handleButtonPress}> 
                         <Text style={styles.buttonText}>Salvar</Text> 
@@ -55,7 +79,7 @@ export default function Cadastrar( {navigation} ) {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 0,
+      flex: 1,
       backgroundColor: '#f7f7f7',
       alignItems: 'center',
     },
@@ -66,7 +90,7 @@ const styles = StyleSheet.create({
       marginTop: 50,
     },
     inputContainer: {
-      flex: 0,
+      flex: 1,
       marginTop: 30,
       width: '90%',
       padding: 20,
@@ -83,10 +107,10 @@ const styles = StyleSheet.create({
       alignItems: 'stretch'
     },
     button: {
-      marginTop: 10,
+      marginTop: 15,
       height: 60,
-      backgroundColor: 'blue',
-      borderRadius: 10,
+      backgroundColor: '#4CAF50',
+      borderRadius: 5,
       paddingHorizontal: 24,
       fontSize: 16,
       alignItems: 'center',
