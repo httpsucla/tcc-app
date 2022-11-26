5b5d6842f54b3ede4df21e39a50e83d38f5a1e3c
Angelo1704 <angelogabrielleite@gmail.com>
2022-11-26 19:18:06 -0300

    funcionalidades gaveta

--- tcc-front/src/pages/Gaveta/components/Box/index.js
+++ tcc-front/src/pages/Gaveta/components/Box/index.js
@@ -2,18 +2,38 @@ import React, { useState } from 'react';
 import { View, TouchableOpacity, Modal, Text, Pressable } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import styles from './style';
+import { useNavigation } from '@react-navigation/native';
+import { SelectList } from 'react-native-dropdown-select-list';
 
-export default function Box() {
+export default function Box(props) {
+    const navigation = useNavigation();
+    const [ selected, setSelected ] = React.useState("");
+    const [ medicamento, setMedicamento] = React.useState({});
 
-    const [items, setItems] = useState([
-        { id: 1, drug: { nome: "Dorflex", hour: "17:00", qtde: 10, date: "08/08/2022", days: 3 } },
-        { id: 2, drug: { nome: "Dorflex", hour: "17:00", qtde: 10, date: "08/08/2022", days: 3 } },
-        { id: 3, drug: { nome: "Buscopan", hour: "08:30", qtde: 12, date: "08/08/2022", days: 4 } },
-        { id: 4, drug: { nome: "Buscopan", hour: "08:30", qtde: 12, date: "08/08/2022", days: 4 } }
-    ]);
+    
+    const data = [{keys: '1', value: 'Remedio 1'},
+    {keys: '2', value: 'Remedio 2'},
+    {keys: '3', value: 'Remedio 3'},
+    {keys: '5', value: 'Remedio 5'},
+    {keys: '6', value: 'Remedio 6'},
+    {keys: '7', value: 'Remedio 7'},
+    {keys: '8', value: 'Remedio 8'},
+    {keys: '9', value: 'Remedio 9'},
+    {keys: '10', value: 'Remedio 10'},
+    {keys: '11', value: 'Remedio 11'},
+    {keys: '12', value: 'Remedio 12'},
+    {keys: '13', value: 'Remedio 13'},    
+];
+    
     
     async function handleButtonPress() {
         setModalVisible(!modalVisible);
+        navigation.navigate('CreateUpdateMedicamento');
+    }
+    async function salvar(){
+        //enviar const selected para bd e processos de salvar na gaveta
+        setModalVisible(!modalVisible);
+        setMedicamento(props.medicamento.drug);
     }
     const [modalVisible, setModalVisible] = useState(false);
 
@@ -26,22 +46,40 @@ export default function Box() {
                 onRequestClose={() => {
                     Alert.alert("Modal has been closed.");
                     setModalVisible(!modalVisible);
-                }}>
+                }}>   
                 <View style={styles.centeredView}>
                     <View style={styles.modalView}>
-                        <Text style={styles.modalText}>Hello World!</Text>
+                        <Text style={styles.modalText}>  {props.name} </Text>
+                        
+                        
+                    <SelectList
+                        setSelected={(val) => setSelected(val)}
+                        data={data}
+                        save="value"
+                    />
+                        
                         <Pressable
                             style={[styles.button, styles.buttonClose]}
                             onPress={handleButtonPress}
                         >
-                            <Text style={styles.textStyle}>Hide Modal</Text>
+                            <Text style={styles.textStyle}>Adicionar medicamento</Text>
                         </Pressable>
+
+                        <Pressable
+                            style={[styles.button, styles.buttonClose]}
+                            onPress={salvar}
+                        >
+                            <Text style={styles.textStyle}>Salvar</Text>
+                        </Pressable>
+
                     </View>
                 </View>
+                
             </Modal>
             <View style={styles.box}>
                 <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
-                    {Object.keys(items[0].drug).length === 0 ?
+                    {Object.keys(medicamento).length === 0 ?
+                    
                         <Icon name="plus-circle" size={60} color={'#4CAF50'} />
                         :
                         <Icon name="archive" size={60} color={'#b2633a'} />
--- tcc-front/src/pages/Gaveta/index.js
+++ tcc-front/src/pages/Gaveta/index.js
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import styles from './style';
 import Box from './components/Box';
 
 export default function Gaveta({ navigation, route }) {
 
+    const [items, setItems] = useState([
+        { id: 1, drug: { nome: "Dorflex", hour: "17:00", qtde: 10, date: "08/08/2022", days: 3 } },
+        { id: 2, drug: { nome: "Dorflex", hour: "17:00", qtde: 10, date: "08/08/2022", days: 3 } },
+        { id: 3, drug: { nome: "Buscopan", hour: "08:30", qtde: 12, date: "08/08/2022", days: 4 } },
+        { id: 4, drug: { nome: "Buscopan", hour: "08:30", qtde: 12, date: "08/08/2022", days: 4 } }
+    ]);
+
     return (
         <View style={styles.container}>
             <View style={styles.flex}>
                 <View style={styles.gaveta}>
                     <Text style={styles.title}>Gaveta 1</Text>
-                    <Box />
+                    <Box name = "Gaveta 1" medicamento = {items[0]} />
                 </View>
                 <View style={styles.gaveta}>
                     <Text style={styles.title}>Gaveta 2</Text>
-                    <Box />
+                    <Box name = "Gaveta 2" medicamento = {items[1]} />
                 </View>
             </View>
             <View style={styles.flex}>
                 <View style={styles.gaveta}>
                     <Text style={styles.title}>Gaveta 3</Text>
-                    <Box />
+                    <Box name = "Gaveta 3" medicamento = {items[2]}/>
                 </View>
                 <View style={styles.gaveta}>
                     <Text style={styles.title}>Gaveta 4</Text>
-                    <Box />
+                    <Box name = "Gaveta 4" medicamento = {items[3]}/>
                 </View>
             </View>
         </View>
