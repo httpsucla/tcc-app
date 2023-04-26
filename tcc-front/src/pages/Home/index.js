47c680a50cfd1ef2e3f998af87514c4470c73e7e
httpsucla <paulasucla@hotmail.com>
2023-04-26 00:40:14 -0300

    logica do proximo medicamento a ser tomado

--- tcc-front/src/pages/Home/index.js
+++ tcc-front/src/pages/Home/index.js
@@ -1,37 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { Text, View, ScrollView, TouchableOpacity } from 'react-native';
 import styles from './style';
 import DatabaseManager from '../../services/testDb';
 import { BarChart, LineChart } from 'react-native-chart-kit'
-
+import moment from 'moment';
 
 export default function Home() {
 
-    const [medicamentos, setMedicamentos] = useState([]);
-    const [gavetas, setGavetas] = useState([]);
-
-    const g = [
-      //  { nome: 'Gaveta ' + (gavetas[0].id+1), medicamento: medicamentos[0].nome_medicamento, qtde: medicamentos[0].qtde_medicamento },
-        { nome: 'Gaveta 1', medicamento: 'Remedio teste 1', qtde: 18 },
-        { nome: 'Gaveta 2', medicamento: 'Remedio teste 2', qtde: 3 },
-        { nome: 'Gaveta 3', medicamento: 'Remedio teste 3', qtde: null },
-        { nome: 'Gaveta 4', medicamento: 'Remedio teste 4', qtde: 4 },
-    ]
-
     useEffect(() => {
-
-        DatabaseManager.getGavetas((gavetas) => {
+        DatabaseManager.joinGavetaMedicamento((gavetas) => {
             setGavetas(gavetas);
         });
-        
-        console.log("seila");
 
-        DatabaseManager.joinGavetaMedicamento((medicamentos) =>{
+        DatabaseManager.getMedicamentos((medicamentos) => {
             setMedicamentos(medicamentos);
         });
-        console.log("embaixo")
-        console.log(medicamentos);
+        ultimoMed();
     }, []);
+
+    const [medicamentos, setMedicamentos] = useState([]);
+    const [gavetas, setGavetas] = useState([]);
+    const [listaMed, setListaMed] = useState([]);
+
+    const g = [
+        gavetas[0] ?
+            { nome: 'Gaveta ' + (gavetas[0].id + 1), medicamento: gavetas[0].nome_medicamento, qtde: gavetas[0].qtde_medicamento }
+            : { nome: 'Gaveta 1', medicamento: '', qtde: null },
+        gavetas[1] ?
+            { nome: 'Gaveta ' + (gavetas[1].id + 1), medicamento: gavetas[1].nome_medicamento, qtde: gavetas[1].qtde_medicamento }
+            : { nome: 'Gaveta 2', medicamento: '', qtde: null },
+        gavetas[2] ?
+            { nome: 'Gaveta ' + (gavetas[2].id + 1), medicamento: gavetas[2].nome_medicamento, qtde: gavetas[2].qtde_medicamento }
+            : { nome: 'Gaveta 3', medicamento: '', qtde: null },
+        gavetas[3] ?
+            { nome: 'Gaveta ' + (gavetas[3].id + 1), medicamento: gavetas[3].nome_medicamento, qtde: gavetas[3].qtde_medicamento }
+            : { nome: 'Gaveta 4', medicamento: '', qtde: null },
+    ]
 
     const data = {
         labels: [g[0].nome, g[1].nome, g[2].nome, g[3].nome],
@@ -52,16 +56,66 @@ export default function Home() {
         ]
     }
 
+    const ultimoMed = () => {
+
+        const lastMed = [];
+
+        medicamentos.forEach((medicamento) => {
+            const dateObj = new Date(medicamento.data_inicial) // transforma a data inicial em Date
+            const timeObj = new Date(`1970-01-01T${medicamento.horario}000Z`); // transforma o horario inicial em date
+            const now = new Date();
+            const combinedDate = new Date(dateObj.setHours(timeObj.getHours(), timeObj.getMinutes(), timeObj.getSeconds())); // junta a data e o horario em um novo objeto
+
+            const dataInicio = moment(combinedDate).format(); // data inicio do tratamento
+            const agora = moment(now); // momento atual
+
+            const diasDecorridos = agora.diff(dataInicio, 'days'); // quantos dias ja passaram desde o inicio do tratamento
+            const diasFaltantes = medicamento.qtde_dias - diasDecorridos; // quantos dias faltam para terminar o tratamento
+            const mediaHoras = 24 / (parseInt(medicamento.qtde) / parseInt(medicamento.qtde_dias)); // calcula de quanto em quanto tempo deve-se engerir o medicamento
+            let horarioProximo = moment(new Date(now.setHours(timeObj.getHours(), timeObj.getMinutes(), timeObj.getSeconds())));
+            horarioProximo.add(mediaHoras, 'hours'); // calcula os futuros horarios que o usuario deverá tomar o medicamento
+
+            while (horarioProximo.isBefore(agora)) // enquanto o horario do dia for menor que o horario do dia atual, incrementa o horario
+                horarioProximo.add(mediaHoras, 'hours');
+
+            if (horarioProximo.isAfter(agora)) {
+                lastMed.push({ // cria objeto com o horario mais proximo de cada medicamento
+                    id: medicamento.id,
+                    title: medicamento.nome,
+                    DataAtual: horarioProximo,
+                    Horario: String(moment.utc(horarioProximo).format('hh:mm')),
+                });
+            }
+            console.log(agora)
+
+            lastMed.sort((a, b) => { // ordena a lista por ordem crescente de data
+                const dataA = new Date(a.DataAtual);
+                const dataB = new Date(b.DataAtual);
+                if (dataA < dataB) {
+                    return -1;
+                } else if (dataA > dataB) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
+            setListaMed(lastMed);
+            console.log(lastMed)
+        });
+    }
+
     return (
         <ScrollView>
             <View style={styles.container}>
-                <View style={styles.boxHour}>
-                    <Text style={styles.boxHourText}>Próximo horário às 18:00</Text>
-                </View>
+                <TouchableOpacity style={styles.boxHour} onPress={ultimoMed}>
+                    <View >
+                        <Text style={styles.boxHourText}>Próximo horário às {listaMed[0] ? listaMed[0].Horario : ''}</Text>
+                    </View>
+                </TouchableOpacity>
                 <View style={styles.boxesMedicamento}>
                     <View style={[styles.box, styles.boxData]}>
                         <View style={styles.boxContent}>
-                            <Text style={styles.valorContent}>20</Text>
+                            <Text style={styles.valorContent}>22</Text>
                             <Text style={styles.diasContent}> dias</Text>
                         </View>
                         <Text style={styles.descricao}>Sequência de dias</Text>
@@ -77,7 +131,7 @@ export default function Home() {
                 <View style={styles.boxesMedicamento}>
                     <View style={[styles.box, styles.boxNome]}>
                         <View style={styles.boxContent}>
-                            <Text style={styles.valorContent}>{g[0].medicamento}</Text>
+                            <Text style={styles.valorContent}>{listaMed.title}</Text>
                         </View>
                         <Text style={styles.descricao}>Último medicamento tomado</Text>
                     </View>
